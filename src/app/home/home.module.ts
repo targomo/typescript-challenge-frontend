c75aac17e3696747343a1ec1f649f9a13b0b2396
Jan Silbersiepe <jan.silbersiepe@targomo.com>
2021-04-07 17:17:18 +0200

    added some styling

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { MatRippleModule } from '@angular/material/core'
-import { MatListModule } from '@angular/material/list'
+import { MatIconModule } from '@angular/material/icon'
 import { HomeComponent } from './home.component'
 
 @NgModule({
   declarations: [HomeComponent],
   exports: [HomeComponent],
-  imports: [CommonModule, MatListModule, MatRippleModule],
+  imports: [CommonModule, MatIconModule],
 })
 export class HomeModule {}
